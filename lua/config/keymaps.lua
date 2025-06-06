38c96c1b6d2c107282eb325399a625a87a9072c9
kimheffy <kimjeffrey96@gmail.com>
2025-06-06 00:43:19 -0700

    Add groups to which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Dir in Oil" })
-vim.keymap.set("n", "gl", function()
+vim.keymap.set("n", "<leader>gl", function()
 	vim.diagnostic.open_float()
 end, { desc = "Open Diagnostic float" })
 
@@ -8,3 +8,85 @@ vim.keymap.set("n", "<leader>cf", function()
 		lsp_format = "fallback",
 	})
 end, { desc = "Format current file" })
+
+-- move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- split panes
+vim.keymap.set("n", "<leader>|", "<cmd>vsplit<CR>", { desc = "Split pane vertical" })
+vim.keymap.set("n", "<leader>-", "<cmd>split<CR>", { desc = "Split pane horizontal" })
+
+-- buffers
+vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+vim.keymap.set(
+	"n",
+	"<leader>ur",
+	"<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+	{ desc = "Redraw / Clear hlsearch / Diff Update" }
+)
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- location list
+vim.keymap.set("n", "<leader>xl", function()
+	local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
+	if not success and err then
+		vim.notify(err, vim.log.levels.ERROR)
+	end
+end, { desc = "Location List" })
+
+-- quickfix list
+vim.keymap.set("n", "<leader>xq", function()
+	local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
+	if not success and err then
+		vim.notify(err, vim.log.levels.ERROR)
+	end
+end, { desc = "Quickfix List" })
+
+vim.keymap.set("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
+vim.keymap.set("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
+
+-- formatting
+vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+	LazyVim.format({ force = true })
+end, { desc = "Format" })
+
+-- diagnostic
+local diagnostic_goto = function(next, severity)
+	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+	severity = severity and vim.diagnostic.severity[severity] or nil
+	return function()
+		go({ severity = severity })
+	end
+end
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -1,16 +1,16 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "echasnovski/mini.icons" },
-  opts = {},
-  keys = {
-    {"<leader>ff", "<cmd>FzfLua files<CR>", { desc = "Find file in current dir"}},
-    {"<leader>fg", "<cmd>FzfLua live_grep<CR>", { desc = "Find by grepping current dir"}},
-    {"<leader>fc", "<cmd>FzfLua files cwd=~/.config<CR>", { desc = "Find in neovim config"}},
-    {"<leader>fw", "<cmd>FzfLua grep_cword<CR>", { desc = "Find current word"}},
-    {"<leader>fd", "<cmd>FzfLua diagnostics_document<CR>", { desc = "Find Diagnostics"}},
-    {"<leader>fr", "<cmd>FzfLua resume<CR>", { desc = "Find resume"}},
-    {"<leader>fo", "<cmd>FzfLua oldfiles<CR>", { desc = "Find old files"}},
-    {"<leader><leader>", "<cmd>FzfLua buffers<CR>", { desc = "Find buffers"}},
-    {"<leader>/", "<cmd>FzfLua lgrep_curbuf<CR>", { desc = "Live grep cur buffer"}},
-  }
+	"ibhagwan/fzf-lua",
+	dependencies = { "echasnovski/mini.icons" },
+	opts = {},
+	keys = {
+		{ "<leader>ff", "<cmd>FzfLua files<CR>", desc = "[f]ind [f]ile" },
+		{ "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "[f]ind live [g]rep" },
+		{ "<leader>fc", "<cmd>FzfLua files cwd=~/.config<CR>", desc = "[f]ind [c]onfig" },
+		{ "<leader>fw", "<cmd>FzfLua grep_cword<CR>", desc = "[f]ind grep [w]ord" },
+		{ "<leader>fd", "<cmd>FzfLua diagnostics_document<CR>", desc = "[f]ind [d]iagnostic" },
+		{ "<leader>fr", "<cmd>FzfLua resume<CR>", desc = "[f]ind [r]esume" },
+		{ "<leader>fo", "<cmd>FzfLua oldfiles<CR>", desc = "[f]ind [o]ld files" },
+		{ "<leader><leader>", "<cmd>FzfLua buffers<CR>", desc = "[f]ind buffer" },
+		{ "<leader>/", "<cmd>FzfLua lgrep_curbuf<CR>", desc = "[f]ind live grep" },
+	},
 }
